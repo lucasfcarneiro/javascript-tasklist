234f7efa2fd1e2dcd251183786e5e9e8d081111c
Lucas Fagundes Carneiro <>
2023-08-07 20:57:52 -0300

    delete implemented

--- js/control.js
+++ js/control.js
@@ -1,21 +1,25 @@
 let inputTask = document.getElementById('inputTask');
 let buttonAdd = document.getElementById('buttonAdd');
 let main = document.getElementById('listArea');
+let counter = 0;
+
 
 function addTask(){
     let inputValue = inputTask.value;
 
     if ((inputValue !== "") && (inputValue!== null) && (inputValue !== undefined)){
         
-        let newItem = `<div class="item">
-        <div class="item-icon">
+        ++counter;
+
+        let newItem = `<div id="${counter}" class="item">
+        <div onclick= "selectTask(${counter})" class="item-icon">
         <i class="fa-regular fa-circle"></i>
         </div>
-        <div class="item-name">
+        <div onclick= "selectTask(${counter})" class="item-name"> 
         ${inputValue}
         </div>
         <div class="item-button">
-            <button class="delete"><i class ="fa-solid fa-trash"></i>Deletar</button>
+            <button onclick="deleteTask(${counter})" class="delete"><i class ="fa-solid fa-trash"></i>Deletar</button>
         </div>
     </div>`;
 
@@ -25,6 +29,16 @@ function addTask(){
     inputTask.focus();
 
     }
+}
+
+function deleteTask(id){
+
+    let task = document.getElementById(id);
+    task.remove();
+}
+
+function selectTask(id){
+    
 }
 
 inputTask.addEventListener("keyup", function(event){
